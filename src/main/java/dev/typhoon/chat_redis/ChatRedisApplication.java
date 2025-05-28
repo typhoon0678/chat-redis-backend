08d331bef06a054de73fcf0d8cc931096a663410
typhoon0678 <typhoon0678@gmail.com>
2025-05-28 20:27:37 +0900

    feat: 초기 세팅

---
+++ src/main/java/dev/typhoon/chat_redis/ChatRedisApplication.java
@@ -0,0 +1,13 @@
+package dev.typhoon.chat_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/typhoon/chat_redis/ChatRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.typhoon.chat_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
